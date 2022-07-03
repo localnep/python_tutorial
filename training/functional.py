5303feee97d08f30c884395c294e44adae414810
Ozan <75545621+localnep@users.noreply.github.com>
2022-07-03 03:12:11 +0300

    Create functional.py

---
+++ training/functional.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  2 22:25:01 2022
+
+@author: erdog
+"""
+
+###############################################
+# lambda, map, filter, reduce - functional programming - Vektör Seviyesinde İşlemler
+###############################################
+
+"""
+lambda: Kullan at fonksiyonu, özellikle apply ve map ile birlikte kullanılır
+map: fonksiyon verilerek döngü yazmaktan kurtarır (java'da foreach yapısına benzer)
+filter: “true” dönen elemanlar için bir liste oluştururken
+reduce: genel olarak bir liste üzerinde işlemler yapmak için kullanılır.                                           
+"""
+
+def new_salary(x): #verilen maaş'a %20 zam yapar
+    return x * 20 / 100 + x
+
+
+#lambda
+new_sum = lambda x: x * 20 / 100 + x
+
+
+#map(function_name, list)
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+list(map(new_salary, salaries))
+
+
+#lambda - map use together
+list(map(lambda x: x * 20 / 100 + x, salaries)) #salaries listesindeki elemanlara %20 zam yapar
+
+#filter
+list_store = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list(filter(lambda x: x % 2 == 0, list_store)) # [2,4,6,8,10]
+
+#reduce
+from functools import reduce
+list_store = [1, 2, 3, 4]
+reduce(lambda a, b: a + b, list_store) #list store'daki elemanları toplar
